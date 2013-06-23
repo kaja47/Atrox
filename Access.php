34b3ac05395d21ff5efc0cf00dc7ca11b0174e44
K <kaja47@k47.cz>
2013-06-24 00:10:44 +0200

    added Atrox\Access

---
+++ Access.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Atrox;
+
+/**
+ * Class for generating accessor functions.
+ * Usage: https://gist.github.com/kaja47/5846724
+ */
+final class Access implements \ArrayAccess {
+
+  private $f;
+
+  function __construct($f = null) {
+    $this->f = ($f === null) ? function ($x) { return $x; } : $f;
+  }
+
+  function __get($name) {
+    return new Access(function ($x) use ($name) {
+      return $this($x)->$name;
+    });
+  }
+
+  function __call($name, $args) {
+    return new Access(function ($x) use ($name, $args) {
+      return call_user_func_array([$this($x), $name], $args);
+    });
+  }
+
+  function offsetGet($offset) {
+    return new Access(function ($x) use ($name, $offset) {
+      return $this($x)[$offset];
+    });
+  }
+
+  function offsetSet($offset, $val) {
+    throw new \BadMethodCallException("unsupported operation");
+  }
+
+  function offsetExists($offset) {
+    throw new BadMethodCallException("unsupported operation");
+  }
+
+  function offsetUnset($offset) {
+    throw new BadMethodCallException("unsupported operation");
+  }
+
+  /** executes accessor function */
+  function __invoke($args) {
+    return call_user_func($this->f, $args);
+  }
+}
